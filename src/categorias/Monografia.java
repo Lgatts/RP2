ef6424314785931bff28793653afcd5c4b50ed38
Lgatts <Lgatts@gmail.com>
2016-08-09 10:09:43 -0300

    Revertendo a modificação no inicial.

--- src/categorias/Monografia.java
+++ src/categorias/Monografia.java
@@ -27,6 +27,35 @@ public class Monografia {
 
     /**
      *
+     * @param titulo
+     * @param situacao
+     * @param tipo
+     * @param autor
+     * @param instituicao
+     * @param orientador
+     * @param curso
+     * @param ano
+     * @param numeroDePaginas
+     * @param palavraChave
+     * @param resumo
+     * @param abstractText
+     */
+    public Monografia(String titulo, String situacao, String tipo, String[] autor, String instituicao, String orientador, String curso, int ano, int numeroDePaginas, String[] palavraChave, String resumo, String abstractText) {
+        this.titulo = titulo;
+        this.situacao = situacao;
+        this.tipo = tipo;
+        this.autor = autor;
+        this.instituicao = instituicao;
+        this.orientador = orientador;
+        this.curso = curso;
+        this.ano = ano;
+        this.numeroDePaginas = numeroDePaginas;
+        this.palavraChave = palavraChave;
+        this.resumo = resumo;
+        this.abstractText = abstractText;
+    }
+    /**
+     *
      * @return
      */
     public String getTitulo() {
@@ -119,51 +148,54 @@ public class Monografia {
      */
     public String getAbstractText() {
         return abstractText;
+    }    
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
-    /**
-     *
-     * @param titulo
-     * @param situacao
-     * @param tipo
-     * @param autor
-     * @param instituicao
-     * @param orientador
-     * @param curso
-     * @param ano
-     * @param numeroDePaginas
-     * @param palavraChave
-     * @param resumo
-     * @param abstractText
-     */
-    public Monografia(String titulo, String situacao, String tipo, String[] autor, String instituicao, String orientador, String curso, int ano, int numeroDePaginas, String[] palavraChave, String resumo, String abstractText) {
-        this.titulo = titulo;
+    public void setSituacao(String situacao) {
         this.situacao = situacao;
+    }
+
+    public void setTipo(String tipo) {
         this.tipo = tipo;
+    }
+
+    public void setAutor(String[] autor) {
         this.autor = autor;
+    }
+
+    public void setInstituicao(String instituicao) {
         this.instituicao = instituicao;
+    }
+
+    public void setOrientador(String orientador) {
         this.orientador = orientador;
+    }
+
+    public void setCurso(String curso) {
         this.curso = curso;
+    }
+
+    public void setAno(int ano) {
         this.ano = ano;
+    }
+
+    public void setNumeroDePaginas(int numeroDePaginas) {
         this.numeroDePaginas = numeroDePaginas;
+    }
+
+    public void setPalavraChave(String[] palavraChave) {
         this.palavraChave = palavraChave;
+    }
+
+    public void setResumo(String resumo) {
         this.resumo = resumo;
+    }
+
+    public void setAbstractText(String abstractText) {
         this.abstractText = abstractText;
     }
-    
-    public void adicionar(){
-        
-    }
-    
-    public void consultar(){
-        
-    }
-
-    public void excluir(){
-        
-    }
-    
-    public void editar(){
-        
-    }
+     
 }
--- src/frames/Inicial.java
+++ src/frames/Inicial.java
@@ -31,56 +31,21 @@ public class Inicial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jAdicionarButton = new javax.swing.JButton();
-        jTituloTextField = new javax.swing.JTextField();
-        jTituloLabel = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jComboBox2 = new javax.swing.JComboBox<>();
-        jLabel3 = new javax.swing.JLabel();
-        jAutorTextField = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jInstTextField = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
-        jOrientadorTextField = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
-        jCursoTextField = new javax.swing.JTextField();
-        jAnoTextField = new javax.swing.JTextField();
-        jLabel7 = new javax.swing.JLabel();
-        jNPaginasTextField = new javax.swing.JTextField();
-        jLabel8 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButtonShortCourse = new javax.swing.JButton();
+        jLabelMenu = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jAdicionarButton.setText("Adicionar");
-        jAdicionarButton.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("Monografias");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jAdicionarButtonActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
-
-        jTituloLabel.setText("Titulo");
-
-        jLabel1.setText("Situação");
-
-        jLabel2.setText("Tipo");
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
-        jLabel3.setText("Autor");
-
-        jLabel4.setText("Instituição");
-
-        jLabel5.setText("Orientador");
-
-        jLabel6.setText("Curso");
-
-        jLabel7.setText("Ano");
-
-        jLabel8.setText("Nº Paginas");
 
         jButtonShortCourse.setText("Minicursos");
         jButtonShortCourse.addActionListener(new java.awt.event.ActionListener() {
@@ -103,113 +68,51 @@ public class Inicial extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(50, 50, 50)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(10, 10, 10)
-                        .addComponent(jLabel3)
-                        .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(174, 174, 174)
+                        .addComponent(jLabelMenu))
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel1))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(103, 103, 103))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(89, 89, 89))))))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jAutorTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 356, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jOrientadorTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 356, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jInstTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 356, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(160, 160, 160)
-                            .addComponent(jAdicionarButton))
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(64, 64, 64)
-                            .addComponent(jTituloLabel))
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(21, 21, 21)
-                            .addComponent(jTituloTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 356, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addComponent(jCursoTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 356, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(jAnoTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(77, 77, 77)
-                            .addComponent(jNPaginasTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(32, Short.MAX_VALUE))
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(45, 45, 45)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel4)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(48, 48, 48)
-                        .addComponent(jLabel6))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(40, 40, 40)
-                        .addComponent(jLabel7)
-                        .addGap(106, 106, 106)
-                        .addComponent(jLabel8)))
-                .addGap(0, 0, Short.MAX_VALUE))
+                        .addGap(104, 104, 104)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButtonShortCourse, javax.swing.GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
+                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(103, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabelMenu)
                 .addGap(18, 18, 18)
-                .addComponent(jTituloLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTituloTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(10, 10, 10)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jAutorTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jOrientadorTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(16, 16, 16)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jInstTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jCursoTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(12, 12, 12)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(jLabel8))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jAnoTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jNPaginasTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 114, Short.MAX_VALUE)
-                .addComponent(jAdicionarButton)
-                .addContainerGap())
+                .addComponent(jButton4)
+                .addGap(18, 18, 18)
+                .addComponent(jButton3)
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addGap(18, 18, 18)
+                .addComponent(jButtonShortCourse)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jAdicionarButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jAdicionarButtonActionPerformed
-        
-    }//GEN-LAST:event_jAdicionarButtonActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        //Monografia monografia = new Monografia(titulo, situacao, tipo, autor, instituicao, orientador, curso, ano, nPaginas, palavraChave, resumo, abstractText)
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButtonShortCourseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonShortCourseActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+        PanelShortCourse openWindow = new PanelShortCourse();
+        openWindow.setVisible(true);
+    }//GEN-LAST:event_jButtonShortCourseActionPerformed
 
     /**
      * @param args the command line arguments
@@ -247,24 +150,11 @@ public class Inicial extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jAdicionarButton;
-    private javax.swing.JTextField jAnoTextField;
-    private javax.swing.JTextField jAutorTextField;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
-    private javax.swing.JTextField jCursoTextField;
-    private javax.swing.JTextField jInstTextField;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JTextField jNPaginasTextField;
-    private javax.swing.JTextField jOrientadorTextField;
-    private javax.swing.JLabel jTituloLabel;
-    private javax.swing.JTextField jTituloTextField;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButtonShortCourse;
+    private javax.swing.JLabel jLabelMenu;
     // End of variables declaration//GEN-END:variables
 }
