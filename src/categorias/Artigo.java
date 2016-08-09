18234c0ffe1cba63cbb6023c961f51aac74d1106
Mauricio Esobar <mauriiosescobar@gmail.com>
2016-08-09 20:21:22 -0300

    Artigo start

---
+++ src/categorias/Artigo.java
@@ -0,0 +1,69 @@
+package categorias;
+
+public class Artigo {
+
+    private String titulo;
+    private String situacao;
+    private String autor;
+    private String instituicao;
+    private String palavrachave;
+    private String resumo;
+    private String abstrat;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getInstituicao() {
+        return instituicao;
+    }
+
+    public void setInstituicao(String instituicao) {
+        this.instituicao = instituicao;
+    }
+
+    public String getPalavrachave() {
+        return palavrachave;
+    }
+
+    public void setPalavrachave(String palavrachave) {
+        this.palavrachave = palavrachave;
+    }
+
+    public String getResumo() {
+        return resumo;
+    }
+
+    public void setResumo(String resumo) {
+        this.resumo = resumo;
+    }
+
+    public String getAbstrat() {
+        return abstrat;
+    }
+
+    public void setAbstrat(String abstrat) {
+        this.abstrat = abstrat;
+    }
+
+}
