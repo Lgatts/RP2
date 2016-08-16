555a2d1a6609888f3bfb55041fff0ca27b0bf72f
LucasCorrea <LucasCorrea@10.2.12.131>
2016-08-16 16:18:46 -0300

    Revert "Revert "Add e Listar , Novamente porque domingo não foi acho..  ""
    This reverts commit 40d4e9150b02c9d819447af6c34024db95b02c51.

--- src/cruds/PalestrasCrud.java
+++ src/cruds/PalestrasCrud.java
@@ -1,45 +1,43 @@
-
 package cruds;
+
 import categorias.Palestras;
 import categorias.ShortCourse;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.DefaultListModel;
+
 /**
  *
  * @author LucasCorrea
  */
 public class PalestrasCrud {
-    
-     private static List<Palestras> PalestrasLista = new ArrayList<>();
-     
-     
-     
-     public static void inserirpalestra(String titulo, String situacao, String autor, String resumo, String abstracText, String duracao, String curriculo){
+
+    private static List<Palestras> PalestrasLista = new ArrayList<>();
+
+    public static void inserirpalestra(String titulo, String situacao, String autor, String resumo, String abstracText, String duracao, String curriculo) {
         Palestras palestra = new Palestras(titulo, situacao, autor, resumo, abstracText, duracao, curriculo);
         PalestrasLista.add(palestra);
-     
-     }
-     
-     public static List<String> listarpalestra(String titulo){
-         List<String> consultaLista = new ArrayList();
-         if(titulo.trim().equals("")){
-             for(Palestras palestra : PalestrasLista){
-                 consultaLista.add(palestra.getTitulo());
-             }
+
+    }
+
+    public static List<String> listarpalestra(String titulo) {
+        List<String> consultaLista = new ArrayList();
+
+        if (titulo.trim().equals("")) {
+            for (Palestras palestra : PalestrasLista) {
+                consultaLista.add(palestra.getTitulo());
+            }
+        } else {
+            for (Palestras palestra : PalestrasLista) {
+                if(palestra.getTitulo().trim().equalsIgnoreCase(titulo)){
+                palestra.getTitulo().trim().equalsIgnoreCase(titulo);
+                consultaLista.add(palestra.getTitulo());
+                }
+                }
+
         }
-         else{
-         for (Palestras palestra : PalestrasLista){
-            palestra.getTitulo().trim().equalsIgnoreCase(titulo);
-            consultaLista.add(palestra.getTitulo());
-         }
-     
-     }
-         return consultaLista;
-     }
-     public static List gLista(){
-     
-     return PalestrasLista;
-     }
+        return consultaLista;
+    }
+
 }
 
