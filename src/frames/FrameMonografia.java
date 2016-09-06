95209e8a960453c257e54f19ed89a606d99a3f72
Lgatts <Lgatts@gmail.com>
2016-09-06 19:22:02 -0300

    Monografia setando os combobox com os enums

--- src/frames/FrameMonografia.java
+++ src/frames/FrameMonografia.java
@@ -43,6 +43,16 @@ public class FrameMonografia extends javax.swing.JFrame {
 
         jButtonEditarMonografia.setEnabled(false);
         jButtonExcluirMonografia.setEnabled(false);
+        
+        for (Situacao e: Situacao.values()){
+            jComboBoxSituacao.addItem(e.getSituacao());
+            jComboBoxSituacaoEditar.addItem(e.getSituacao());
+        }
+        for (Tipo e : Tipo.values()){
+            jComboBoxTipo.addItem(e.getTipo());
+            jComboBoxTipoEditar.addItem(e.getTipo());
+        }
+        
 
     }
 
@@ -283,7 +293,7 @@ public class FrameMonografia extends javax.swing.JFrame {
                     .addGroup(jPanelListarLayout.createSequentialGroup()
                         .addGap(11, 11, 11)
                         .addComponent(jButtonConsulta)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jScrollPaneMonografias, javax.swing.GroupLayout.PREFERRED_SIZE, 644, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(30, 30, 30))
         );
@@ -346,10 +356,7 @@ public class FrameMonografia extends javax.swing.JFrame {
             }
         });
 
-        jComboBoxSituacao.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Sob avaliação", "Aprovado", "Reprovado" }));
         jComboBoxSituacao.setToolTipText("");
-
-        jComboBoxTipo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Graduação", "Especialização", "Mestrado", "Doutorado" }));
 
         jLabelCurso.setText("Curso");
 
@@ -497,7 +504,7 @@ public class FrameMonografia extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(76, Short.MAX_VALUE))
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
         jPanel1Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jTextFieldPalavraChave0, jTextFieldPalavraChave1, jTextFieldPalavraChave2, jTextFieldPalavraChave3});
@@ -577,10 +584,7 @@ public class FrameMonografia extends javax.swing.JFrame {
             }
         });
 
-        jComboBoxSituacaoEditar.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Sob avaliação", "Aprovado", "Reprovado" }));
         jComboBoxSituacaoEditar.setToolTipText("");
-
-        jComboBoxTipoEditar.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Graduação", "Especialização", "Mestrado", "Doutorado" }));
 
         jLabelCursoEditar.setText("Curso");
 
@@ -724,7 +728,7 @@ public class FrameMonografia extends javax.swing.JFrame {
                 .addGroup(jPanelEditar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane8, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jScrollPane7, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(62, Short.MAX_VALUE))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
 
         jPanelEditar1Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jScrollPane7, jScrollPane8});
