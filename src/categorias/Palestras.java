1f44229a88f959547b1c9e4ac043d26543a3bab2
yuryalencar <yuryalencar@yuryalencar>
2016-09-06 17:03:00 -0300

    Palestras - Adaptação das classes e melhoria da frame, mas ainda falta algumas coisas. Próximo passo - adequar à interface.

--- src/categorias/Palestras.java
+++ src/categorias/Palestras.java
@@ -2,14 +2,8 @@
 package categorias;
 
 
-public class Palestras {
+public class Palestras extends SubmissaoApresentacao{
     
-    private String titulo;
-    private String situacao;
-    private String autor;
-    private String resumo;
-    private String abstracText;
-    private String duracao;
     private String curriculo;
     
     /**
@@ -26,102 +20,14 @@ public class Palestras {
    /*Construtor*/
     
  
-    public Palestras(String titulo, String situacao, String autor, String resumo, String abstracText, String duracao, String curriculo) {
-        this.titulo = titulo;
-        this.situacao = situacao;
-        this.autor = autor;
-        this.resumo = resumo;
-        this.abstracText = abstracText;
-        this.duracao = duracao;
+    public  Palestras(String titulo, Situacao situacao, String autor, String resumo, String abstracText, int duracao, String curriculo) {
+        super(titulo, duracao, 1, resumo, abstracText);
         this.curriculo = curriculo;
         
     }
    
    /*Getter and Setter*/
-   /**
-    * 
-    * @return 
-    */
-    public String getTitulo() {
-        return titulo;
-    }
-    /**
-     * 
-     * @param titulo 
-     */
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    /**
-     * 
-     * @return 
-     */
-    public String getSituacao() {
-        return situacao;
-    }
-    /**
-     * 
-     * @param situacao 
-     */
-    public void setSituacao(String situacao) {
-        this.situacao = situacao;
-    }
-    /**
-     * 
-     * @return 
-     */
-    public String getAutor() {
-        return autor;
-    }
-    /**
-     * 
-     * @param autor 
-     */
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-    /**
-     * 
-     * @return 
-     */
-    public String getResumo() {
-        return resumo;
-    }
-    /**
-     * 
-     * @param resumo 
-     */
-    public void setResumo(String resumo) {
-        this.resumo = resumo;
-    }
-    /**
-     * 
-     * @return 
-     */
-    public String getAbstracText() {
-        return abstracText;
-    }
-    /**
-     * 
-     * @param abstracText 
-     */
-    public void setAbstracText(String abstracText) {
-        this.abstracText = abstracText;
-    }
-    /**
-     * 
-     * @return 
-     */
-    public String getDuracao() {
-        return duracao;
-    }
-    /**
-     * 
-     * @param duracao 
-     */
-    public void setDuracao(String duracao) {
-        this.duracao = duracao;
-    }
+   
     /**
      * 
      * @return 
--- src/categorias/Tipo.java
+++ src/categorias/Tipo.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package categorias;
+
 /*
 Graduação
 Especialização
@@ -14,42 +15,45 @@ Doutorado
  * @author Junior
  */
 public enum Tipo {
-    GRADUAÇAO("Graduação"), 
-    ESPECIALIZAÇÃO("Especialização"), 
-    MESTRADO("Mestrado"), 
+    GRADUAÇAO("Graduação"),
+    ESPECIALIZAÇÃO("Especialização"),
+    MESTRADO("Mestrado"),
     DOUTORADO("Doutorado");
-    
+
     private final String tipo;
+
     /**
-     * 
-     * @param tipo 
+     *
+     * @param tipo
      */
-    private Tipo(String tipo){
-        this.tipo = tipo;    
+    private Tipo(String tipo) {
+        this.tipo = tipo;
     }
-/**
- * 
- * @return Tipo
- */
+
+    /**
+     *
+     * @return Tipo
+     */
     public String getTipo() {
         return tipo;
     }
+
     /**
-     * 
+     *
      * @param tipo
-     * @return 
+     * @return
      */
-    public static Tipo verificarTipo(String tipo){
-        if(Tipo.DOUTORADO.getTipo().equals(tipo)){
+    public static Tipo verificarTipo(String tipo) {
+        if (Tipo.DOUTORADO.getTipo().equals(tipo)) {
             return Tipo.DOUTORADO;
-        }else if(Tipo.ESPECIALIZAÇÃO.getTipo().equals(tipo)){
+        } else if (Tipo.ESPECIALIZAÇÃO.getTipo().equals(tipo)) {
             return Tipo.ESPECIALIZAÇÃO;
-        }else if(Tipo.MESTRADO.getTipo().equals(tipo)){
+        } else if (Tipo.MESTRADO.getTipo().equals(tipo)) {
             return Tipo.MESTRADO;
-        }else if(Tipo.GRADUAÇAO.getTipo().equals(tipo)){
+        } else if (Tipo.GRADUAÇAO.getTipo().equals(tipo)) {
             return Tipo.GRADUAÇAO;
-        }else{
-    return null;
+        } else {
+            return null;
         }
     }
 }
